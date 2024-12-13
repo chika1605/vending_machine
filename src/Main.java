985513a609c68ba6f3af9e67f8ad878a416dba8c
chika <152640307+chika1605@users.noreply.github.com>
2024-12-13 15:50:23 +0600

    Refactor Main class to use BillAcceptor for payment processing in AppRunner

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import model.BillAcceptor;
+import model.CardAcceptor;
+import model.CoinAcceptor;
+
 public class Main {
     public static void main(String[] args) {
-        AppRunner.run();
+//        AppRunner.run(new CoinAcceptor(0));
+//        AppRunner.run(new CardAcceptor(0));
+        AppRunner.run(new BillAcceptor(0));
+
+
+
     }
 }
